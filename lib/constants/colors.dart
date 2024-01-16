1d003c20d113e48ea944934764171584239403d4
jashp143 <jashprajapati143@gmail.com>
2024-01-16 16:15:25 +0530

    initial comit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Colors.grey;
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Colors.black87;
+
+const Color tdBGColor = Colors.black87;
